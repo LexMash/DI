02171c5bdc434f909af849df7ea98b61ff584770
LexMash <>
2024-07-23 10:20:28 +0300

    interface fix

--- Assets/Scripts/BaCon/Container/IDIResolver.cs
+++ Assets/Scripts/BaCon/Container/IDIResolver.cs
@@ -7,6 +7,13 @@ namespace BaCon
     {
         T Resolve<T>(string tag = null);
         IReadOnlyList<T> ResolveAll<T>();
+
+#if UNITY_2017_3_OR_NEWER && NET_4_6
+        void ResolveAllHierarchy<T>(T instance) where T : MonoBehaviour;
+
+        void ResolveAllHierarchy(GameObject gameObject);
+#endif
+
         T InstantiateAndResolve<T>(GameObject prefab, string tag = null);
         T InstantiateAndResolve<T>(T prefab, string tag = null) where T : MonoBehaviour;
     }
